869fd2cd6c9e14d69b293b63f66f9728b49bc8b4
Chintan Soni <chintansoni202@gmail.com>
2016-10-01 00:28:33 +0530

    Initial commit

---
+++ app/src/main/java/com/androidveteran/android/firebasecrashreporting/MainActivity.java
@@ -0,0 +1,79 @@
+package com.androidveteran.android.firebasecrashreporting;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.crash.FirebaseCrash;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Report Exception: My First self-generated exception", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                FirebaseCrash.report(new Exception("My First self-generated exception"));
+            }
+        });
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Log will not show in Android Monitor, but will show in Crash Report", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                FirebaseCrash.log("My First Log");
+            }
+        });
+
+        FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Log will be showed in both Android Monitor and Crash Report", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                FirebaseCrash.logcat(Log.ERROR, "MainActivity", "My First Logcat");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
